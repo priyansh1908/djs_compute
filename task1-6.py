657ef7a97e55116a859385a7d39b4c73e77b89ae
priyansh1908 <63773317+priyansh1908@users.noreply.github.com>
2023-09-24 13:19:52 +0530

    Add files via upload

---
+++ task1-6.py
@@ -0,0 +1,13 @@
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+print("X is:",numbers_x)
+print("Y is:",numbers_y)
+if numbers_x[0]== numbers_x[-1]:
+    print("first and last same numbers in X")
+else:
+    print("first and last not same numbers in X")
+    
+if numbers_y[0]== numbers_y[-1]:
+    print("first and last same numbers in Y")
+else:
+    print("first and last not same numbers in Y")
